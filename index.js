8a1d1aaf4cc01e3e87f6de7fd9d07576433f99c4
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2025-12-15 12:47:05 +0800

    new: add layout ui

---
+++ index.js
@@ -0,0 +1,7 @@
+function add(){
+    document.getElementById("add-panel").style.display = "flex";
+}
+
+function cancelAddPanel(){
+    document.getElementById("add-panel").style.display = "none";
+}
